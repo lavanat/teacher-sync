6f4f555d2b55cb2b0b20ff1bd3eff23312b579ac
Lavanya Natchiappan <lavee.natch@gmail.com>
2023-04-05 17:57:58 -0700

    fixed schema and seeds file to source correctly

--- db/schema.sql
+++ db/schema.sql
@@ -3,41 +3,41 @@ CREATE DATABASE teacher_db;
 
 USE teacher_db;
 
---Parent Tabel
+-- Parent Table
 CREATE TABLE students (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   student_name VARCHAR(100) NOT NULL
 );
 
---Child Table
+-- Child Table
 CREATE TABLE grades (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     class_date DATE NOT NULL,
     grade INT NOT NULL,
-    student_id INT NOT NULL
+    student_id INT,
     FOREIGN KEY (student_id)
     REFERENCES students(id)
     ON DELETE SET NULL
-)
+);
 
---Child Table
+-- Child Table
 CREATE TABLE attendance (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     class_date DATE NOT NULL,
     attendance VARCHAR (100) NOT NULL,
-    student_id INT NOT NULL
+    student_id INT,
     FOREIGN KEY (student_id)
     REFERENCES students(id)
     ON DELETE SET NULL
-)
+);
 
---Conduct
+-- Child Table
 CREATE TABLE conduct (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     class_date DATE NOT NULL,
     conduct VARCHAR (1) NOT NULL,
-    student_id INT NOT NULL
+    student_id INT,
     FOREIGN KEY (student_id)
     REFERENCES students(id)
     ON DELETE SET NULL
-)
+);
