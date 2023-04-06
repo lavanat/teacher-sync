2fdc0b31c1451c013b39516fb12e7a46d1086a4c
Lavanya Natchiappan <lavee.natch@gmail.com>
2023-04-06 16:41:12 -0700

    added models with Pierre

---
+++ models/conduct.js
@@ -0,0 +1,38 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Conduct extends Model {}
+
+Conduct.init(
+    {
+        id: {
+          type: DataTypes.INTEGER,
+          allowNull: false,
+          primaryKey: true,
+          autoIncrement: true,
+        },
+        class_date: {
+          type: DataTypes.DATEONLY,
+          allowNull: false,
+        },
+        conduct: {
+          type: DataTypes.CHAR,
+        },
+        student_id: {
+          type: DataTypes.INTEGER,
+          references: {
+            model: 'student',
+            key: 'id',
+          },
+        },
+      },
+      {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'conduct',
+      }
+);
+
+module.exports = Conduct;
