1c9ee1314e189de7ac92950dab0f375820c60f82
PoonamBurris <119805763+PoonamBurris@users.noreply.github.com>
2023-04-07 18:32:34 -0400

    Created Login/Logout/Register-handlebars

---
+++ controllers/views/homeRoutes.js
@@ -0,0 +1,62 @@
+const router = require('express').Router();
+const withAuth = require('../../utils/auth');
+
+router.get('/', async (req, res) => {
+
+    if (req.session.logged_in) {
+        console.log('ALREADY LOGGED IN')
+        res.redirect('/grades');
+        return
+    }
+
+    try {
+    //   // Get all projects and JOIN with user data
+    //   const projectData = await Project.findAll({
+    //     include: [
+    //       {
+    //         model: User,
+    //         attributes: ['name'],
+    //       },
+    //     ],
+    //   });
+  
+      // Serialize data so the template can read it
+    //   const projects = projectData.map((project) => project.get({ plain: true }));
+  
+      // Pass serialized data and session flag into template
+      res.render('homepage', { 
+        logged_in: req.session.logged_in 
+      });
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
+  router.get('/login', (req, res) => {
+    // If the user is already logged in, redirect the request to another route
+    if (req.session.logged_in) {
+      res.redirect('/grades');
+      return;
+    }
+  
+    res.render('login');
+  });
+  
+  router.get('/register', (req, res) => {
+    // If the user is already logged in, redirect the request to another route
+    if (req.session.logged_in) {
+      res.redirect('/grades');
+      return;
+    }
+  
+    res.render('register');
+  });
+
+  router.get('/grades', withAuth, async (req, res) => {
+      res.render('grades', {
+        logged_in: req.session.logged_in,
+        teacher_name: req.session.teacher_name
+      })
+  })
+
+  module.exports = router;
--- models/index.js
+++ models/index.js
@@ -1,3 +1,4 @@
 const User = require('./user');
+const Teacher = require('./teacher')
 
-module.exports = { User };
+module.exports = { User, Teacher };
