c69a6784308082d280e980a3a3bcc14982b69761
Lavanya Natchiappan <lavee.natch@gmail.com>
2023-04-13 09:54:00 -0700

    deleted personal css files and folders and changed css back to Poonam's code

--- Carl-assets/date.js
+++
@@ -1,4 +0,0 @@
-const dateControl = document.querySelector('input[type="date"]');
-dateControl.value = '2017-06-01';
-console.log(dateControl.value); // prints "2017-06-01"
-console.log(dateControl.valueAsNumber); // prints 1496275200000, a JavaScript timestamp (ms)
